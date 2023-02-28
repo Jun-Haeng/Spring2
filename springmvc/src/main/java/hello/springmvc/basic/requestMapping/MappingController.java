65e971275351e55099091ad374871a266e33bc4b
Jun-Haeng <gjwnsgod123@hanmail.net>
2023-02-28 17:24:37 +0900

    0228 study

---
+++ springmvc/src/main/java/hello/springmvc/basic/requestMapping/MappingController.java
@@ -0,0 +1,33 @@
+package hello.springmvc.basic.requestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+public class MappingController {
+
+    @RequestMapping(value ="/hello-basic", method = RequestMethod.GET)
+    public String mappingGetV1() {
+        log.info("helloBasic");
+        return "ok";
+    }
+
+    @GetMapping(value = "/mapping-get-v2")
+    public String mappingGetV2() {
+        log.info("mappingGetV2");
+        return "ok";
+    }
+
+    /**
+     *
+     * @return
+     */
+
+    @GetMapping("/mapping/{userId}")
+    public String mappingPath(@PathVariable("userId") String data) {
+        log.info("mappingPath userId={}", data);
+        return "ok";
+    }
+
+}
